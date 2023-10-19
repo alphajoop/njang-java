be76a18e24f7397df106f03ec423a17cfa10be57
Alpha DIOP <alphakhoss@gmail.com>
2023-10-19 12:00:05 +0000

    init: helloWord

---
+++ Njang.java
@@ -0,0 +1,10 @@
+/** Ceci est une implémentation du message traditionnel "Hello world!"
+ * @author Alpha JOOB
+ */
+public class Njang {
+
+  /** Le programme commence ici */
+  public static void main(String[] args) {
+    System.out.println("Hello word !");
+  }
+}
